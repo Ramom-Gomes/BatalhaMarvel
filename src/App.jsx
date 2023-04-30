cbb9188803ebfdf6150e74db2214ed747c0605f3
Ramom Gomes <ramomusica09@hotmail.com>
2023-04-30 20:55:14 -0300

    time azul template conclusão

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,55 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <h1>marvel</h1>
-    </>
+    <main className='main'>
+      <section className='timeazul'>
+        <div className='timeazulcaracteristicas'>
+          <div>
+            <h1 className='personagemnome'>nome personagem</h1>
+          </div>
+          <div className='personagenscaracteristicasfundo'>
+            <div className='personagenscaracteristicas'>
+              <div className='personagenscaracteristicaespecifica'><p className='personagemcaracteristicatitulo'>força</p><p>5</p></div>
+              <div className='personagenscaracteristicaespecifica'><p className='personagemcaracteristicatitulo'>força</p><p>5</p></div>
+              <div className='personagenscaracteristicaespecifica'><p className='personagemcaracteristicatitulo'>força</p><p>5</p></div>
+              <div className='personagenscaracteristicaespecifica'><p className='personagemcaracteristicatitulo'>força</p><p>5</p></div>
+              <div className='personagenscaracteristicaespecifica'><p className='personagemcaracteristicatitulo'>força</p><p>5</p></div>
+              <div className='personagenscaracteristicaespecifica'><p className='personagemcaracteristicatitulo'>força</p><p>5</p></div>
+              <div className='personagenscaracteristicaespecifica'><p className='personagemcaracteristicatitulo'>força</p><p>5</p></div>
+              <div className='personagenscaracteristicaespecifica'><p className='personagemcaracteristicatitulo'>força</p><p>5</p></div>
+              <div className='personagenscaracteristicaespecifica2'><p>Poder total</p><p>5</p></div>
+            </div>
+          </div>
+        </div>
+        <div className='timeazulpersonagens'>
+          <div className='timeazulescolherpersonagem'>
+            <div>
+              <h1 className='timeazultitulo'>time azul</h1>
+              <p className='timeazulinfo'>escolha três personagens</p>
+            </div>
+            <div className='timeazulescolhadepersonagens'>
+              <div className='timeazulpersonagensparaescolher'>personagem</div>
+              <div className='timeazulpersonagensparaescolher'>personagem</div>
+              <div className='timeazulpersonagensparaescolher'>personagem</div>
+              <div className='timeazulpersonagensparaescolher'>personagem</div>
+              <div className='timeazulpersonagensparaescolher'>personagem</div>
+              <div className='timeazulpersonagensparaescolher'>personagem</div>
+            </div>
+          </div>
+          <div className='timeazulpersonagensescolhidos'>
+            <h1 className='timeazulescolhidostitulo'>escolhidos</h1>
+            <div className='timeazulescolhidos'>
+              <div className='timeazulescolhidosposicao'>escolhido 1</div>
+              <div className='timeazulescolhidosposicao'>escolhido 2</div>
+              <div className='timeazulescolhidosposicao2'>escolhido 3</div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='timevermelho'>
+        <h1>vermelho</h1>
+      </section>
+    </main>
   )
 }
 
