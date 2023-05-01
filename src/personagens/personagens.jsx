6993bb71a9fdc7b20d669b21b18802ee3999c5cb
Ramom Gomes <ramomusica09@hotmail.com>
2023-05-01 18:30:42 -0300

    personagens

---
+++ src/personagens/personagens.jsx
@@ -0,0 +1,151 @@
+import React, { useState } from "react";
+
+export const timeCapitaoAmerica = [
+    {
+      nome: "Capitão América",
+      habilidades: {
+        forcaFisica: 95,
+        velocidade: 60,
+        agilidade: 60,
+        resistencia: 90,
+        inteligencia: 70,
+        habilidadeCombate: 100,
+        poderesEspeciais: 70,
+        nivelExperiencia: 95,
+      },
+    },
+    {
+      nome: "Falcão",
+      habilidades: {
+        forcaFisica: 70,
+        velocidade: 90,
+        agilidade: 90,
+        resistencia: 80,
+        inteligencia: 60,
+        habilidadeCombate: 80,
+        poderesEspeciais: 50,
+        nivelExperiencia: 70,
+      },
+    },
+    {
+      nome: "Soldado Invernal",
+      habilidades: {
+        forcaFisica: 90,
+        velocidade: 70,
+        agilidade: 70,
+        resistencia: 90,
+        inteligencia: 60,
+        habilidadeCombate: 90,
+        poderesEspeciais: 70,
+        nivelExperiencia: 90,
+      },
+    },
+    {
+      nome: "Homem-Formiga",
+      habilidades: {
+        forcaFisica: 75,
+        velocidade: 70,
+        agilidade: 80,
+        resistencia: 70,
+        inteligencia: 80,
+        habilidadeCombate: 70,
+        poderesEspeciais: 75,
+        nivelExperiencia: 60,
+      },
+    },
+    {
+      nome: "Gavião Arqueiro",
+      habilidades: {
+        forcaFisica: 75,
+        velocidade: 60,
+        agilidade: 70,
+        resistencia: 80,
+        inteligencia: 60,
+        habilidadeCombate: 80,
+        poderesEspeciais: 60,
+        nivelExperiencia: 80,
+      },
+    },
+    {
+      nome: "Feiticeira Escarlate",
+      habilidades: {
+        forcaFisica: 70,
+        velocidade: 60,
+        agilidade: 70,
+        resistencia: 70,
+        inteligencia: 90,
+        habilidadeCombate: 70,
+        poderesEspeciais: 95,
+        nivelExperiencia: 70,
+      },
+    },
+];
+
+export const timeHomemDeFerro = [
+    {
+      nome: "Homem de Ferro",
+      forcaFisica: 80,
+      velocidade: 60,
+      agilidade: 70,
+      resistencia: 80,
+      inteligencia: 100,
+      habilidadeCombate: 90,
+      poderesEspeciais: 85,
+      nivelExperiencia: 90
+    },
+    {
+      nome: "Máquina de Combate",
+      forcaFisica: 75,
+      velocidade: 50,
+      agilidade: 60,
+      resistencia: 80,
+      inteligencia: 80,
+      habilidadeCombate: 80,
+      poderesEspeciais: 75,
+      nivelExperiencia: 85
+    },
+    {
+      nome: "Viúva Negra",
+      forcaFisica: 60,
+      velocidade: 70,
+      agilidade: 90,
+      resistencia: 70,
+      inteligencia: 80,
+      habilidadeCombate: 95,
+      poderesEspeciais: 75,
+      nivelExperiencia: 90
+    },
+    {
+      nome: "Pantera Negra",
+      forcaFisica: 90,
+      velocidade: 80,
+      agilidade: 90,
+      resistencia: 85,
+      inteligencia: 80,
+      habilidadeCombate: 95,
+      poderesEspeciais: 80,
+      nivelExperiencia: 95
+    },
+    {
+      nome: "Visão",
+      forcaFisica: 80,
+      velocidade: 70,
+      agilidade: 80,
+      resistencia: 80,
+      inteligencia: 100,
+      habilidadeCombate: 85,
+      poderesEspeciais: 95,
+      nivelExperiencia: 85
+    },
+    {
+      nome: "Homem-Aranha",
+      forcaFisica: 75,
+      velocidade: 85,
+      agilidade: 95,
+      resistencia: 70,
+      inteligencia: 80,
+      habilidadeCombate: 85,
+      poderesEspeciais: 80,
+      nivelExperiencia: 80
+    }
+];  
