1bc79dc90e69658d3fe82c7df0da30b24e1331e3
Ramom Gomes <ramomusica09@hotmail.com>
2023-05-30 21:34:49 -0300

    mensagem-vencedor

--- src/App.jsx
+++ src/App.jsx
@@ -229,6 +229,7 @@ function App() {
             {mensagemVisivel && (
                 <div className="caixa-mensagem">
                   <p>{resultadoVisivel}</p>
+                  <p>...</p>
                   <button className='botaofecharresultado' onClick={handleFecharResultado}>fechar</button>
                 </div>
             )}
